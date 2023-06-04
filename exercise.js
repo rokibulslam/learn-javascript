db1637612fbd60955880b2464bae45861e5d74df
Reymond Julio <reymondjulio58@gmail.com>
2023-06-04 12:58:22 +0700

    initial exercise javascript

---
+++ exercise.js
@@ -0,0 +1,21 @@
+// Memunculkan nama lengkap
+let firstName = "Reymond";
+let lastName = "Julio";
+let fullName = "Reymond" + " " + "Julio";
+console.log(fullName);
+
+// Memunculkan huruf pertama dari sebuah nama
+let firstCharacter = fullName.charAt(0);
+console.log(firstCharacter);
+
+// Mengubah nama menjadi huruf besar semua
+let nameUpperCase = fullName.toUpperCase();
+console.log(nameUpperCase);
+
+// Mengubah nama menjadi huruf kecil semua
+let nameLowerCase = fullName.toLowerCase();
+console.log(nameLowerCase);
+
+// Menghitung jumlah nama karakter
+let nameLength = fullName.length;
+console.log(nameLength);
