39775b203ab8cfe3a32eac4785864c469a029e9f
Reymond Julio <reymondjulio58@gmail.com>
2023-06-08 13:44:19 +0700

    implement check person age to get national id

--- national-id.js
+++ national-id.js
@@ -4,3 +4,12 @@
 // - Given a person age who was born in Indonesia
 // - Check if the person is already 17 years old or above, they can get a National ID (KTP)
 // - Else if not, then they cannot get it yet
+// - Show the message with they age number
+
+const personAge = 5;
+
+if (personAge >= 17) {
+  console.log(`${personAge} years old can get a National ID`);
+} else if (personAge < 17) {
+  console.log(`${personAge} years old can not get a National ID`);
+}
