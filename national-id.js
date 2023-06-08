4d602f8f61f8487309fef2bae2351ff8ccea39d9
M Haidar Hanif <git@mhaidarhanif.com>
2023-06-08 13:35:20 +0700

    add blank national-id program

---
+++ national-id.js
@@ -0,0 +1,6 @@
+// National ID (KTP)
+
+// Algorithm:
+// - Given a person age who was born in Indonesia
+// - Check if the person is already 17 years old or above, they can get a National ID (KTP)
+// - Else if not, then they cannot get it yet
