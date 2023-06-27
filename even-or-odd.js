1915bb68f26611516300abb4d207c5974376c87f
Reymond Julio <reymondjulio58@gmail.com>
2023-06-27 12:20:02 +0700

    implement function even or odd program

---
+++ even-or-odd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(5));
+console.log(evenOrOdd(4));
+console.log(evenOrOdd(9));
+console.log(evenOrOdd(11));
