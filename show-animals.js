92fc5cc29d4f56f334164b97b76ffd1a2051b44a
Reymond Julio <reymondjulio58@gmail.com>
2023-06-27 12:38:42 +0700

    implement show animals

--- show-animals.js
+++ show-animals.js
@@ -7,28 +7,26 @@ function showAnimals(animals) {
   const isMoreThan2Animals = animals.length > 2;
 
   for (let index = 0; index < animals.length; index++) {
-    const animalsName = animals[index];
+    const animalName = animals[index];
     const isLast = index === animals.length - 1;
 
     if (isLast && isMoreThan1Animal) {
-      sentence += `and ${animalsName}`;
+      sentence += `and ${animalName}`;
     }
     if (isLast && !isMoreThan1Animal) {
-      sentence += `${animalsName}`;
+      sentence += `${animalName}`;
     }
     if (!isLast && isMoreThan2Animals) {
-      sentence += `${animalsName}, `;
+      sentence += `${animalName}, `;
     }
     if (!isLast && !isMoreThan2Animals) {
-      sentence += `${animalsName} `;
+      sentence += `${animalName} `;
     }
   }
+
   return sentence;
 }
 
-const animals = ["Dog", "Cat", "Rabbit", "Tiger", "Lion"];
 console.log(showAnimals(["Dog"]));
 console.log(showAnimals(["Dog", "Cat"]));
 console.log(showAnimals(["Dog", "Cat", "Rabbit"]));
-console.log(showAnimals(["Dog", "Cat", "Rabbit", "Tiger"]));
-console.log(showAnimals(["Dog", "Cat", "Rabbit", "Tiger", "Lion"]));
