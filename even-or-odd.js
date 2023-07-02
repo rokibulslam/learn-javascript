d7814ed4646ed481f898f0b7c704acdeb1c8dce4
Reymond Julio <reymondjulio58@gmail.com>
2023-07-02 14:11:08 +0700

    try the ternary operator

--- even-or-odd.js
+++ even-or-odd.js
@@ -1,9 +1,5 @@
 function evenOrOdd(number) {
-  if (number % 2 === 0) {
-    return "Even";
-  } else {
-    return "Odd";
-  }
+  return number % 2 === 0 ? "Even" : "Odd";
 }
 
 console.log(evenOrOdd(2));
