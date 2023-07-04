4516c1d319838247912044695cb01668931764ea
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-04 13:27:16 +0700

    prepare is total odd or even function

--- even-or-odd.js
+++ even-or-odd.js
@@ -1,5 +1,27 @@
+function isEven(number) {
+  if (number % 2 === 0) return true
+  else return false
+}
+
+function isOdd(number) {
+  if (number % 2 !== 0) return true
+  else return false
+}
+
 function evenOrOdd(number) {
-  return number % 2 === 0 ? "Even" : "Odd";
+  return (isEven(number)) ? "Even" : "Odd";
+}
+
+// one of them is odd, then the total is always odd
+function isTotalOdd(numberA, numberB) {
+  // 3 + 4 = 7 = odd = true
+  // 6 + 3 = 9 = odd = true
+}
+
+// both of them are either odd or even, then the total is always even
+function isTotalEven(numberA, numberB) {
+  // 1 + 3 = 4 = even = true
+  // 2 + 6 = 8 = even = true
 }
 
 console.log(evenOrOdd(2));
