6857dd36ffe79d4140ede9642bd0ca71cdd5e4cd
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-04 13:51:40 +0700

    initialize dom friends

---
+++ dom-friends/index.js
@@ -0,0 +1,11 @@
+const friends = [
+  { name: "Reymond J", age: 26 },
+  { name: "Haidar H", age: 30 },
+  { name: "Julio", age: 25 },
+  { name: "Budi", age: 35 },
+  { name: "Joko", age: 40 },
+];
+
+friends.forEach((friend) => {
+  console.log(friend);
+});
